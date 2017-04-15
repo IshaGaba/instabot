c69d8a9e5fe8da4f56f225f55f48497818bc9f5a
unknown <ishagaba>
2017-04-15 20:48:06 +0530

    fetched user id

--- instabot.py
+++ instabot.py
@@ -2,6 +2,13 @@ import requests
 App_Access_token = "2247652388.61a4075.a8ace6f81850450683de586106907181" #access_token
 BASE_URL = "https://api.instagram.com/v1/" #common url
 
+def going_right_or_wrong(data):
+    right = data['meta']['code']
+    if right == 200:
+        print ("successfully found user id and fetched user's post id ")
+    else:
+        print ("unsuccessful plz check user name again")
+
 
 def info_owner():  # using get to collect owner information
     url_owner = BASE_URL + "users/self/?access_token=" + App_Access_token
@@ -12,3 +19,12 @@ def info_owner():  # using get to collect owner information
     print ("owner bio is " + owner_info["data"]["bio"])
     print ("owner usename is " + owner_info["data"]["username"])
     print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+
+
+def get_user_id_username(user_name):  # get user id
+    url_user = BASE_URL + "users/search?q=" + str(user_name) + "&access_token=" + App_Access_token  # https://api.instagram.com/v1/users/search?q=jack&access_token=ACCESS-TOKEN
+    user_info = requests.get(url_user).json()
+    going_right_or_wrong(user_info)
+    return user_info["data"][0]["id"]
+
